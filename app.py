174df545f8c531b5524c4fd4276058d777419e1b
miki-25 <mkd10a14@gmail.com>
2026-05-15 15:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -561,6 +561,11 @@ if run:
         with st.spinner("⬡ Running deep analysis…"):
 
             prompt = f"""You are a business analyst. Return ONLY valid JSON, no markdown, no extra text.
+                        - Competitors must be DIRECT competitors in the SAME industry and market segment only.
+                        - Use realistic insights and latest search on the web for latest numbers for revenue also and if not available give the ans as not publically available.
+                        - Use company-specific KPIs
+                        - Generate a detailed consulting-style business analysis for:
+
 
 Company: {company_name}
 
